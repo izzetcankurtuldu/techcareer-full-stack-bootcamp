f490e66ccffeb3d45e9fc94d9930c7185e0cb38e
İzzet Can Kurtuldu <can.kurtuldu35@gmail.com>
2023-10-14 12:02:20 +0300

    React In Class Task

---
+++ reactInClassTask/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Navbar from './template-1.html/Navbar';
+import Header from './template-1.html/Header';
+import PageContent from './template-1.html/PageContent';
+import ImageLocation from './template-1.html/ImageLocation';
+import Footer from './template-1.html/Footer';
+
+function App() {
+  return <>
+  <Navbar/>
+  <Header/>
+  <PageContent/>
+  <ImageLocation/>
+  <Footer/>
+  </>
+}
+
+export default App;
